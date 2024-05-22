b94273cd22731e06051d83799b80b36faf14a5d9
KoptevaAnastasiia <150849726+KoptevaAnastasiia@users.noreply.github.com>
2024-05-22 10:14:47 +0300

    Saving and loading from file & Printing the current text

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 int main() {
     char *input = 0;
     size_t bufferSize = 256; // Initial buffer size
+    char filename[100]; // Масив для зберігання назви файлу
+
 
     // Allocate memory for the input buffer
     input = (char *)malloc(bufferSize * sizeof(char));
@@ -23,10 +25,38 @@ int main() {
 
 
 
+    printf("Введіть назву файлу для збереження: ");
+    scanf("%99s", filename); // Читаємо назву файлу, враховуючи межу 99 символів
+
+
+
+
+
     char *newline_position = strchr(input, '\n');
     if (newline_position != NULL) {
         *newline_position = '\0';
     }
+
+    // Відкриття файлу для запису
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Помилка відкриття файлу для запису.\n");
+        return 1;
+    }    // Боже що тут відбувається
+
+
+
+    // Запис рядка у файл
+    fprintf(file, "%s", input);
+
+
+    fclose(file);
+
+    // Повідомлення про успішний запис
+    printf("Текст було записано у файл успішно.\n");
+
+
+
 
     // Print the string
     printf("You entered: %s\n",  input);        ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
